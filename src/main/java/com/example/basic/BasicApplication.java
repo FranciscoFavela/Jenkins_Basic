43a6720cc866d39cf21716d52ee81bbcdef6fd84
FranciscoFavelaNajera <98063834+FranciscoFavelaNajera@users.noreply.github.com>
2022-05-23 16:38:52 -0500

    Update BasicApplication.java

--- src/main/java/com/example/basic/BasicApplication.java
+++ src/main/java/com/example/basic/BasicApplication.java
@@ -2,7 +2,7 @@ package com.example.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 //@EnableEurekaClient
 public class BasicApplication {
